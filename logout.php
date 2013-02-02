dfe29ce982d4fc06b0db1bc9b82c0c351a380110
Anna <innahmarte@gmail.com>
2013-02-02 20:07:08 +0800

    logoutphp

---
+++ logout.php
@@ -0,0 +1,68 @@
+<?php 
+
+session_start();
+
+session_destroy();
+//ends session
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Eyes Crime</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="rvbulalacao" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/bootstrap-responsive.css" rel="stylesheet" />
+	<style type="text/css">
+		body {
+			padding-top: 60px;
+			padding-bottom: 40px;
+		}
+		.sidebar-nav {
+			padding: 9px 0;
+		}
+		section, article, nav, header, footer, figure, figcaption {
+			display: block;
+		}
+	</style>
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				<a class="brand" href="index.php">Eyes Crime</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+							<!--navbar for log out-->
+							<li><a href="login.php">Log in </a><li>
+							<li><a href="announcements.php">Announcements </a><li>
+							<li><a href="view.php">View </a><li>
+							<li><a href="search.php">Search </a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<section class="span12">
+				<div class="hero-unit">
+					<!--data if logged out-->
+					<center><h3>You are now logged out!</h3></center>
+				</div>
+			</section>
+		</div>
+	</div>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
