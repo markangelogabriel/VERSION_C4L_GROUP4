bdbb58f2993d86b95c5647b1e5322f57eae2469b
Anna <innahmarte@gmail.com>
2013-02-07 20:59:59 +0800

    manageaccounts.php

---
+++ manageaccounts.php
@@ -0,0 +1,73 @@
+<?php
+	//start session
+	session_start();	
+	
+	//prevents unautorized access, rootadmin can only access this page
+	if($_SESSION['rootadmin']!=1){
+		header("Location: index.php");
+		exit;
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Eyes Crime</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/bootstrap-responsive.css" rel="stylesheet" />
+	<style type="text/css">
+		body {
+			padding-top: 60px;
+			padding-bottom: 40px;
+		}
+		.sidebar-nav {
+			padding: 9px 0;
+		}
+		section, article, nav, header, footer, figure, figcaption {
+			display: block;
+		}
+	</style>
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				<a class="brand" href="index.php">Eyes Crime</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<!--navbar if root admin-->	
+							<li><a href="manageaccounts.php">Manage Admin Accounts</a></li>		
+							<li><a href="announcements.php">Announcements </a></li>
+							<li><a href="view.php">View </a></li>
+							<li><a href="search.php">Search </a></li>
+							<li><a href="logout.php">Logout</a><br /></li>	
+							<li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<section class="span12">
+				<div class="hero-unit">
+					<!--links related to manage accounts-->
+					<center>
+					<a href="createaccounts.php">Create Administrator Accounts </a><br />
+					<a href="deleteaccounts.php">Delete Administrator Accounts </a>
+					</center>
+				</div>
+			</section>
+		</div>
+	</div>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
