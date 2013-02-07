b2556d52c9862673f4ed2cdc874b9c8872285191
Anna <innahmarte@gmail.com>
2013-02-07 20:59:35 +0800

    logout_revised

--- logout.php
+++ logout.php
@@ -14,7 +14,6 @@ session_destroy();
     <title>Eyes Crime</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="author" content="rvbulalacao" />
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/bootstrap-responsive.css" rel="stylesheet" />
 	<style type="text/css">
